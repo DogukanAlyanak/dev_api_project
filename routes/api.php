16a34e46445fb915ac00307ce2f3a3743765346f
DogukanAlyanak <mail@dogukanalyanak.com>
2024-08-06 16:26:34 +0300

    v0.1.0

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApiController;
 
 Route::get('/', function () {
-    return "ok";
+    return response()->json([
+        'message' => 'ok',
+    ]);
 });
 
-Route::get('/user', function (Request $request) {
+Route::post('/register', [ApiController::class, 'register']);
+
+/* Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-
-
-Route::post('/tokens/create', function (Request $request) {
-    $token = $request->user()->createToken($request->token_name);
-
-    return ['token' => $token->plainTextToken];
-});
+})->middleware('auth:sanctum'); */
