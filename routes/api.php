98b68c9a255dcddec19f94fa9dcfa7bceb4d0f69
DogukanAlyanak <mail@dogukanalyanak.com>
2024-08-06 14:34:49 +0300

    0.0.6

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return "ok";
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
